bccc7b934dec07671bcc4105334bbff92b59b12e
cemregur <48982521+cemregur@users.noreply.github.com>
2023-09-06 01:02:25 +0300

    Update and rename foodfacts.py to glisemic_index.py

---
+++ finalProject/glisemic_index.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import streamlit as st
+
+import food_fact_app
+
+df_ = pd.read_excel("/Users/cemregur/QueensOfData/finalProject/new_spagetti.xlsx")
+df = df_.copy()
+
+df.head()
+df.columns()
+
+
+# Glisemik indeksi hesapla
+def calculate_glycemic_index(row):
+    carbohydrates = row['carbohydrates_value']
+    fiber = row['fiber_value']
+    sugars = row['sugars_value']
+    proteins = row['proteins_value']
+    fats = row['fat_value']
+
+    glycemic_index = (carbohydrates - fiber + 0.1 * sugars + 0.15 * proteins + 0.1 * fats)
+    return glycemic_index
+
+
+# Glisemik indeks sütununu hesapla ve DataFrame'e ekle
+df['glycemic_index'] = df.apply(calculate_glycemic_index, axis=1)
+print(df)
+df.head()
+
+# Literatürde Glisemik indekslerine göre gıdalar
+# düşük (1-55), orta (56-69) ve yüksek (>70) Gİ’li olarak sınıflandırılır.
+
+# Kategorik bir sütun oluşturun
+df['GI_category'] = pd.cut(df['glycemic_index'], bins=[0, 55, 69, float('inf')],
+                           labels=['low_gi', 'medium_gi', 'high_gi'], right=False)
+
+print(df)
