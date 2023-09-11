13653bec8e8028d3df5139daaa17543fdd41df7b
cemregur <cemregur93@gmail.com>
2023-09-11 11:17:15 +0300

    GI alanlarının kaldırılması

--- streamlit/Src/utils.py
+++ streamlit/Src/utils.py
@@ -3,15 +3,15 @@ from pandas import DataFrame
 
 
 def read_food_data():
-    df = pd.read_csv('streamlit/Datasets/df.csv', sep=';', low_memory=False)
+    df = pd.read_csv('Datasets/df.csv', sep=';', low_memory=False)
     return df
 
 
 def read_food_data2():
-    df = pd.read_csv('streamlit/Datasets/df.csv', sep=';', low_memory=False)
+    df = pd.read_csv('Datasets/df.csv', sep=';', low_memory=False)
     return df
 
 
 def read_categories_data():
-    df = pd.read_csv('streamlit/Datasets/df_cat.csv', sep=';', low_memory=False)
+    df = pd.read_csv('Datasets/df_cat.csv', sep=';', low_memory=False)
     return df
