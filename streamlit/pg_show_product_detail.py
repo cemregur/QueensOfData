7904157c40ccf4a7f2ff3ca07a491c0baa4f8785
cemregur <cemregur93@gmail.com>
2023-09-12 19:52:39 +0300

    pg_show_product_detail görünüm düzenlemele

--- streamlit/pg_show_product_detail.py
+++ streamlit/pg_show_product_detail.py
@@ -2,33 +2,75 @@ import streamlit as st
 import pandas as pd
 from Src.utils import *
 
+
 def show_Product_Detail(xproduct_code):
     df = get_product_detail_df(xproduct_code)
     if (df.shape[0] != 1):
         st.error("An error occured with the product code :" + str(xproduct_code))
     else:
-        col1, col2 = st.columns((1,2))
+        col1, col2 = st.columns((1, 2))
         col1.image(df["url"].values[0], caption='', use_column_width=True)
         col2.header(df["product_name_en"].values[0])
-        col2.markdown("Code : " + str(df["code"].values[0]))
-        col2.markdown("Ingredients : " + df["ingredients_text_en"].values[0])
-        col2.markdown("Brands : " + df["brands"].values[0])
-        if (str(df["stores"].values[0]) != ""):
-            col2.markdown("Stores : " + df["stores"].values[0])
+        col2.markdown("**CODE** : " + str(df["code"].values[0]))
+        # split correction  ':'
+        ingredients_text = df["ingredients_text_en"].values[0]
+        if ":" in ingredients_text:
+            ingredient_parts = ingredients_text.split(':')
+            if len(ingredient_parts) > 1:
+                ingredient_value = ingredient_parts[1].strip()
+                col2.markdown("**INGREDIENTS** : " + ingredient_value + "**")
+            else:
+                col2.markdown("**INGREDIENTS** : ...")
         else:
-            col2.markdown("Stores : ...")
-        col2.markdown("Allergens : " + df["allergens"].values[0])
+            col2.markdown("**INGREDIENTS : ...**")
+
+        col2.markdown("**BRAND** : " + df["brands"].values[0].upper())
+        if (str(df["stores"].values[0]) != ""):
+            col2.markdown("**STORES** : " + df["stores"].values[0].upper())
+        else:
+            col2.markdown("**STORES** : ...")
+        col2.markdown("**ALLERGENS** : " + df["allergens"].values[0])
         ########################################################################
         # SCORES
         ########################################################################
         with st.container():
-            col1 , col2, col3  = st.columns(3)
-            col1.success("Nutrition Score : " + df["off:nutriscore_grade"].values[0] )
-            col2.error('NOVA Score : ' + df["off:nova_groups"].values[0], icon="🚨")
-            #col3.success("Glisemic Score : " + str(df["GI_category"].values[0]))
+            col1, col2 = st.columns(2)
+            if pd.isna(df["off:nutriscore_grade"].values[0]):
+                col1.success("**NUTRITION SCORE** : No Information")
+            elif df["off:nutriscore_grade"].values[0] == "E":
+                col1.error("**NUTRITION SCORE** : E", icon="🚨")
+            else:
+                col1.success(f"**NUTRITION SCORE** : " + df["off:nutriscore_grade"].values[0])
+
+            if pd.isna(df["off:nova_groups"].values[0]):
+                col2.error("**NOVA SCORE** : No Information")
+            elif df["off:nova_groups"].values[0] == "4":
+                col2.error("**NOVA SCORE** : 4", icon="🚨")
+            else:
+                col2.success("**NOVA SCORE** : " + df["off:nova_groups"].values[0])
+            # col3.success("Glisemic Score : " + str(df["GI_category"].values[0]))
         #######################################################################
         # ALLERGENS
         ########################################################################
+        # Allergen icons and expressions
+        allergen_icons = {
+            "Allergen_Milk": "🥛",
+            "Allergen_Egg": "🥚",
+            "Allergen_Nut": "🌰",
+            "Allergen_Peanut": "🥜",
+            "Allergen_Gluten": "🌾",
+            "Allergen_Soybeans": "🌱"
+        }
+
+        allergen_expressions = {
+            "Allergen_Milk": "Contains Milk",
+            "Allergen_Egg": "Contains Egg",
+            "Allergen_Nut": "Contains Nuts",
+            "Allergen_Peanut": "Contains Peanuts",
+            "Allergen_Gluten": "Contains Gluten",
+            "Allergen_Soybeans": "Contains Soybeans"
+        }
+
         Allergen_Count = 0
         Allergen_Pozitives_list = []
         Allergen_col_list = [col for col in df.columns if "Allergen" in col]
@@ -39,15 +81,17 @@ def show_Product_Detail(xproduct_code):
         if Allergen_Count > 0:
             col_obj_list = st.columns(Allergen_Count)
             for i, col_object in enumerate(col_obj_list):
-                col_object.error(Allergen_Pozitives_list[i] + " POZITIVE ALARM" , icon="🚨" )
-        if Allergen_Count == 0:
-            # Allerjen bilgisi girilmedi
-            if (df["Allergen_Milk"].values[0] == "2"):
-                col_obj_list = st.columns(6)
-                for i, col_object in enumerate(col_obj_list):
-                    col_object.info(Allergen_col_list[i] + " No Information")
+                allergen_key = Allergen_Pozitives_list[i]
+                allergen_icon = allergen_icons.get(allergen_key, "❓")
+                allergen_expression = allergen_expressions.get(allergen_key, "Unknown allergen")
+                col_object.error(f"{allergen_icon} {allergen_expression}")
 
-def check_allergens(df_product ):
+            # Allerjen seçimi yapıldı ama allergens alanı boş
+        if Allergen_Count == 0 and df["Allergen_Milk"].values[0] == "2":
+            st.info("NO ALLERGEN INFORMATION")
+
+
+def check_allergens(df_product):
     if (df_product["allergens"].values[0] == ""):
         allergens = []
         df_product["Allergen_Milk"] = "2"
@@ -85,11 +129,15 @@ def check_allergens(df_product ):
                 chk = False
         if (chk) & ("Nut" in allergens):
             df_product["Allergen_Nut"] = "1"
+        if (chk) & ("Nuts" in allergens):
+            df_product["Allergen_Nut"] = "1"
         df_product["Allergen_Peanut"] = "0"
         if ("pref_Allergen_Peanut" in st.session_state):
             if (st.session_state.pref_Allergen_Peanut == 0):
                 chk = False
         if (chk) & ("Peanut" in allergens):
+            df_product["Allergen_Peanut"] = "1"
+        if (chk) & ("Peanuts" in allergens):
             df_product["Allergen_Peanut"] = "1"
         df_product["Allergen_Soybeans"] = "0"
         if ("pref_Allergen_Soybeans" in st.session_state):
@@ -97,19 +145,23 @@ def check_allergens(df_product ):
                 chk = False
         if (chk) & ("Soybeans" in allergens):
             df_product["Allergen_Soybeans"] = "1"
+        if (chk) & ("Soybean" in allergens):
+            df_product["Allergen_Soybeans"] = "1"
         return df_product
+
+
 def get_product_detail_df(xproduct_code):
-    #df = read_food_data2()
-    df = pd.read_csv('Datasets/df.csv', sep=';', low_memory=False)
+    df = read_food_data2()
     df.loc[df["allergens"].isnull(), "allergens"] = ""
     df.loc[df["stores"].isnull(), "stores"] = ""
-    df["GI_category"] = 1
+    # df["GI_category"] = 1
 
     cols = ["code", "product_name_en", "brands", "off:nova_groups", "off:nutriscore_grade", "allergens",
             "ingredients_text_en", "stores", "url"]
     df_product = df.loc[df["code"].astype(str) == str(xproduct_code), cols]
     df_product_detail = check_allergens(df_product)
     return df_product_detail
+
 
 def app(xproduct_code):
     show_Product_Detail(xproduct_code)
